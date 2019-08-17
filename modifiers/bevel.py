a5d4fc4dd8a6d0d426b50ee7ca5a5cb272aa068a
Jorijn de Graaf <43251584+bonjorno7@users.noreply.github.com>
2019-08-17 18:24:10 +0200

    modals are cool (#3)
    * experimenting with model operators
    
    finally gonna make the modifiers pie actually useful
    
    * modifier modals coming along nicely
    
    a couple more changes and then perhaps some more features
    after that I'll add some more modifiers to the pie
    
    * making modals good
    
    nice
    gonna add some more features later, and some more modifiers

--- modifiers/bevel.py
+++ modifiers/bevel.py
@@ -2,59 +2,123 @@ import bpy
 
 
 class Bevel(bpy.types.Operator):
-    """Add weighted bevel modifiers to the selected objects"""
-    bl_idname = "bedset.modifier_bevel"
+    bl_options = {"REGISTER", "UNDO", "GRAB_CURSOR", "BLOCKING"}
+    bl_idname = "bedset.bevel"
     bl_label = "(B) Bevel"
     bl_icon = 'MOD_BEVEL'
-    bl_options = {'REGISTER', 'UNDO'}
-
-    width: bpy.props.FloatProperty(
-        name="Width",
-        description="Width of the bevel",
-        default=0.1,
-        min=0,
-        max=1000000,
-        soft_min=0,
-        soft_max=1000,
-        step=1,
-        precision=3,
-        subtype='DISTANCE',
-    )
-
-    segments: bpy.props.IntProperty(
-        name="Segments",
-        description="Number of segments",
-        default=4,
-        min=1,
-        max=1024,
-        soft_min=1,
-        soft_max=64,
-        step=1,
-        subtype='UNSIGNED',
-    )
 
     @classmethod
     def poll(cls, context):
-        return len(context.selected_objects) > 0
+        return context.object is not None
+
+    prop_width: bpy.props.FloatProperty(
+        name="Width", subtype='DISTANCE',
+        description="Width of the bevel",
+        default=0.1, min=0, max=1000000,
+        step=1, precision=3)
+
+    prop_segments: bpy.props.IntProperty(
+        name="Segments", subtype='UNSIGNED',
+        description="Number of segments",
+        default=1, min=1, max=100, step=1)
+
+    def get_mod(self, context):
+        self.mod = None
+
+        for m in context.object.modifiers:
+            if m.type == 'BEVEL':
+                self.mod = m
+                break
+
+        if not self.mod:
+            self.new = True
+            self.mod = context.object.modifiers.new(name="Bevel", type='BEVEL')
+            self.mod.limit_method = 'ANGLE'
+            self.mod.width = self.prop_width
+            self.mod.segments = self.prop_segments
+
+    def invoke(self, context, event):
+        self.new = False
+        self.get_mod(context)
+
+        self.width = self.mod.width
+        self.segments = self.mod.segments
+
+        self.prop_width = self.width
+        self.prop_segments = self.segments
+
+        self.init_width = self.width
+        self.init_segments = self.segments
+
+        self.prev_mouse_x = event.mouse_x
+
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
 
     def execute(self, context):
-        for o in context.selected_objects:
-            mod = None
-
-            for m in o.modifiers:
-                if m.type == 'BEVEL':
-                    mod = m
-                    break
-
-            if not mod:
-                mod = o.modifiers.new(name='Bevel', type='BEVEL')
-
-            mod.width = self.width
-            mod.segments = self.segments
-            mod.limit_method = 'WEIGHT'
-
+        self.get_mod(context)
+        self.mod.width = self.prop_width
+        self.mod.segments = self.prop_segments
         return {'FINISHED'}
 
+    def modal(self, context, event):
+        if event.type in {'LEFTMOUSE', 'ENTER', 'SPACE'}:
+            return {'FINISHED'}
+
+        if event.type == 'A':
+            bpy.ops.object.modifier_apply(modifier=self.mod.name)
+            return {'FINISHED'}
+
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            if self.new:
+                context.object.modifiers.remove(self.mod)
+
+            else:
+                self.mod.width = self.init_width
+                self.mod.segments = self.init_segments
+
+            return {'CANCELLED'}
+
+        elif event.type == 'WHEELUPMOUSE':
+            self.segments += 1
+
+            if self.segments > 100:
+                self.segments = 100
+
+            self.prop_segments = self.segments
+
+        elif event.type == 'WHEELDOWNMOUSE':
+            self.segments -= 1
+
+            if self.segments < 1:
+                self.segments = 1
+
+            self.prop_segments = self.segments
+
+        elif event.type == 'MOUSEMOVE':
+            delta = (event.mouse_x - self.prev_mouse_x) / 500
+
+            if event.shift:
+                delta /= 10
+
+            self.width += delta
+
+            if self.width < 0:
+                self.width = 0
+
+            self.prop_width = self.width
+
+            self.prev_mouse_x = event.mouse_x
+
+        if event.ctrl:
+            self.prop_width = round(self.width, 2 if event.shift else 1)
+        else:
+            self.prop_width = self.width
+
+        self.execute(context)
+
+        return {'RUNNING_MODAL'}
+
     def draw(self, context):
-        self.layout.prop(self, "width")
-        self.layout.prop(self, "segments")
+        self.layout.prop(self, "prop_width")
+        self.layout.prop(self, "prop_segments")
--- modifiers/solidify.py
+++ modifiers/solidify.py
@@ -2,58 +2,124 @@ import bpy
 
 
 class Solidify(bpy.types.Operator):
-    """Add solidify modifiers to the selected objects"""
-    bl_idname = "bedset.modifier_solidify"
+    bl_options = {"REGISTER", "UNDO", "GRAB_CURSOR", "BLOCKING"}
+    bl_idname = "bedset.solidify"
     bl_label = "(Y) Solidify"
     bl_icon = 'MOD_SOLIDIFY'
-    bl_options = {'REGISTER', 'UNDO'}
-
-    thickness: bpy.props.FloatProperty(
-        name="Thickness",
-        description="Thickness of the shell",
-        default=0.1,
-        min=0,
-        max=1000000,
-        soft_min=0,
-        soft_max=1000,
-        step=1,
-        precision=3,
-        subtype='DISTANCE',
-    )
-
-    offset: bpy.props.FloatProperty(
-        name="Offset",
-        description="Offset the thickness from the center",
-        default=-1,
-        min=-1,
-        max=1,
-        step=1,
-        precision=3,
-        subtype='FACTOR',
-    )
 
     @classmethod
     def poll(cls, context):
-        return len(context.selected_objects) > 0
+        return context.object is not None
+
+    prop_thickness: bpy.props.FloatProperty(
+        name="Thickness", subtype='DISTANCE',
+        description="Thickness of the shell",
+        default=0.1, min=0, max=1000000,
+        step=1, precision=3)
+
+    prop_offset: bpy.props.FloatProperty(
+        name="Offset", subtype='FACTOR',
+        description="Offset the thickness from the center",
+        default=-1, min=-1, max=1,
+        step=1, precision=3)
+
+    def get_mod(self, context):
+        self.mod = None
+
+        for m in context.object.modifiers:
+            if m.type == 'SOLIDIFY':
+                self.mod = m
+                break
+
+        if not self.mod:
+            self.new = True
+            self.mod = context.object.modifiers.new(name="Solidify", type='SOLIDIFY')
+            self.mod.use_even_offset = True
+            self.mod.thickness = self.prop_thickness
+            self.mod.offset = self.prop_offset
+
+    def invoke(self, context, event):
+        self.new = False
+        self.get_mod(context)
+
+        self.thickness = self.mod.thickness
+        self.offset = self.mod.offset
+
+        self.prop_thickness = self.thickness
+        self.prop_offset = self.offset
+
+        self.init_thickness = self.thickness
+        self.init_offset = self.offset
+
+        self.prev_mouse_x = event.mouse_x
+
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
 
     def execute(self, context):
-        for o in context.selected_objects:
-            mod = None
-
-            for m in o.modifiers:
-                if m.type == 'SOLIDIFY':
-                    mod = m
-                    break
-
-            if not mod:
-                mod = o.modifiers.new(name='Solidify', type='SOLIDIFY')
-
-            mod.thickness = self.thickness
-            mod.offset = self.offset
-            mod.use_even_offset = True
-
+        self.get_mod(context)
+        self.mod.thickness = self.prop_thickness
+        self.mod.offset = self.prop_offset
         return {'FINISHED'}
 
+    def modal(self, context, event):
+        if event.type in {'LEFTMOUSE', 'ENTER', 'SPACE'}:
+            return {'FINISHED'}
+
+        if event.type == 'A':
+            bpy.ops.object.modifier_apply(modifier=self.mod.name)
+            return {'FINISHED'}
+
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            if self.new:
+                context.object.modifiers.remove(self.mod)
+
+            else:
+                self.mod.thickness = self.init_thickness
+                self.mod.offset = self.init_offset
+
+            return {'CANCELLED'}
+
+        elif event.type == 'WHEELUPMOUSE':
+            self.offset += 1
+
+            if self.offset > 1:
+                self.offset = 1
+
+            self.prop_offset = self.offset
+
+        elif event.type == 'WHEELDOWNMOUSE':
+            self.offset -= 1
+
+            if self.offset < -1:
+                self.offset = -1
+
+            self.prop_offset = self.offset
+
+        elif event.type == 'MOUSEMOVE':
+            delta = (event.mouse_x - self.prev_mouse_x) / 500
+
+            if event.shift:
+                delta /= 10
+
+            self.thickness += delta
+
+            if self.thickness < 0:
+                self.thickness = 0
+
+            self.prop_thickness = self.thickness
+
+            self.prev_mouse_x = event.mouse_x
+
+        if event.ctrl:
+            self.prop_thickness = round(self.thickness, 2 if event.shift else 1)
+        else:
+            self.prop_thickness = self.thickness
+
+        self.execute(context)
+
+        return {'RUNNING_MODAL'}
+
     def draw(self, context):
-        self.layout.prop(self, "thickness")
-        self.layout.prop(self, "offset")
+        self.layout.prop(self, "prop_thickness")
+        self.layout.prop(self, "prop_offset")
