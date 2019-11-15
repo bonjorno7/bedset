9e924e14c94665cc44c3052ddaf7d38c1b9b4d15
bonjorno7 <jorijndegraaf@gmail.com>
2019-11-15 20:16:59 +0100

    Minor improvements to boolean modifiers
    Just to get rid of those redundant functions, they're not called often enough to warrant existing

--- booleans/mod.py
+++ booleans/mod.py
@@ -15,7 +15,7 @@ class ModBoolean(bpy.types.Operator):
             ('DIFFERENCE', "Difference", "Remove selected from active"),
             ('UNION', "Union", "Add selected to active"),
             ('INTERSECT', "Intersect", "Intersect selected with active"),
-            ('EXTRACT', "Extract", "Extract selected from active"),
+            ('SLASH', "Slash", "Separate selected from active"),
             ('INSET', "Inset", "Create an inset into active from selected"),
         ),
     )
@@ -27,44 +27,50 @@ class ModBoolean(bpy.types.Operator):
             return len(context.selected_objects) > 1
         return False
 
-    def duplicate(self, source, name):
-        obj = source.copy()
-        obj.name = name + " " + source.name
-        col = source.users_collection[0]
-        col.objects.link(obj)
-        obj.select_set(False)
-        return obj
-
-    def make_inset(self, source):
-        obj = self.duplicate(source, "Inset")
-        obj.display_type = 'WIRE'
-        obj.hide_render = True
-        return obj
-
-    def make_extract(self, source):
-        return self.duplicate(source, "Extract")
-
-    def make_cutter(self, obj):
-        obj.name = "Cutter" + " " + obj.name
-        obj.display_type = 'WIRE'
-        obj.hide_render = True
-
     def execute(self, context):
         active = context.active_object
         cutters = [o for o in context.selected_objects if o is not active]
 
         if self.kind in ['DIFFERENCE', 'UNION', 'INTERSECT']:
             for cutter in cutters:
-                self.make_cutter(cutter)
+                cutter.display_type = 'WIRE'
+                cutter.hide_render = True
                 boolean = active.modifiers.new(name="Boolean", type='BOOLEAN')
                 boolean.operation = self.kind
                 boolean.object = cutter
                 boolean.show_expanded = False
 
+        elif self.kind == 'SLASH':
+            for cutter in cutters:
+                cutter.display_type = 'WIRE'
+                cutter.hide_render = True
+
+                extract = active.copy()
+                collection = active.users_collection[0]
+                collection.objects.link(extract)
+                extract.select_set(False)
+
+                intersect = extract.modifiers.new(name="Boolean", type='BOOLEAN')
+                intersect.operation = 'INTERSECT'
+                intersect.object = cutter
+                intersect.show_expanded = False
+
+                difference = active.modifiers.new(name="Boolean", type='BOOLEAN')
+                difference.operation = 'DIFFERENCE'
+                difference.object = cutter
+                difference.show_expanded = False
+
         elif self.kind == 'INSET':
             for cutter in cutters:
-                self.make_cutter(cutter)
-                inset = self.make_inset(active)
+                cutter.display_type = 'WIRE'
+                cutter.hide_render = True
+
+                inset = active.copy()
+                collection = active.users_collection[0]
+                collection.objects.link(inset)
+                inset.select_set(False)
+                inset.display_type = 'WIRE'
+                inset.hide_render = True
 
                 solidify = inset.modifiers.new(name="Solidify", type='SOLIDIFY')
                 solidify.use_even_offset = True
@@ -80,21 +86,6 @@ class ModBoolean(bpy.types.Operator):
                 difference = active.modifiers.new(name="Boolean", type='BOOLEAN')
                 difference.operation = 'DIFFERENCE'
                 difference.object = inset
-                difference.show_expanded = False
-
-        elif self.kind == 'EXTRACT':
-            for cutter in cutters:
-                self.make_cutter(cutter)
-                extract = self.make_extract(active)
-
-                intersect = extract.modifiers.new(name="Boolean", type='BOOLEAN')
-                intersect.operation = 'INTERSECT'
-                intersect.object = cutter
-                intersect.show_expanded = False
-
-                difference = active.modifiers.new(name="Boolean", type='BOOLEAN')
-                difference.operation = 'DIFFERENCE'
-                difference.object = cutter
                 difference.show_expanded = False
 
         active.select_set(False)
